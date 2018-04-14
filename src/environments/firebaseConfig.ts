5c7a16107d615f6168057408a7b7e3bb5707f920
Eduardo Elias Saleh <eduardo@lab15.io>
2018-04-14 20:29:50 +0200

    initial commit

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,11 @@
+
+
+export const firebaseConfig = {
+    // Paste all this from the Firebase console...
+    apiKey: "AIzaSyAl_8DWvGFUHOa19zh75BbN4-2_gcZlyuE",
+    authDomain: "okr1-d742a.firebaseapp.com",
+    databaseURL: "https://okr1-d742a.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "867750249111"
+};
+
