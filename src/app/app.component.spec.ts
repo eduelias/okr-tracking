24773ce777383efce73a6ca170fd3c59bec6b191
Eduardo Elias Saleh <eduardo@lab15.io>
2018-04-14 20:51:51 +0200

    fixing tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 /* tslint:disable:no-unused-variable */
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import {TestBed, async} from "@angular/core/testing";
-import {AppComponent} from "./app.component";
-import {AuthService} from "app/shared/auth.service";
-import {AuthServiceStub} from "app/shared/auth.service.stub";
-import {AlertModule} from "ngx-bootstrap";
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { AuthService } from "app/shared/auth.service";
+import { AuthServiceStub } from "app/shared/auth.service.stub";
+import { AlertModule } from "ngx-bootstrap";
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
     beforeEach(() => {
@@ -16,10 +17,13 @@ describe('AppComponent', () => {
             ],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
             imports: [
+                RouterTestingModule.withRoutes(
+                    []
+                ),
                 AlertModule.forRoot()
             ],
             providers: [
-                {provide: AuthService, useValue: authServiceStub}
+                { provide: AuthService, useValue: authServiceStub }
             ]
         });;
     });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import {Component} from "@angular/core";
-import {AuthService} from "app/shared/auth.service";
+import { Component } from "@angular/core";
+import { AuthService } from "app/shared/auth.service";
 import { Observable } from "rxjs/Observable";
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
-import {Router} from "@angular/router";
-import {UserInfo} from "app/shared/user-info";
+import { Router } from "@angular/router";
+import { UserInfo } from "app/shared/user-info";
 
 @Component({
     selector: 'app-root',
